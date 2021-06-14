4e30d6b5fc650000bfb8af0d0bcf386dcfe20510
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-06-14 15:39:37 +0200

    changed: updated for Elgg 4

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -1,7 +1,21 @@
 <?php
 
-use ColdTrick\ScrollToTop\Bootstrap;
-
 return [
-	'bootstrap' => Bootstrap::class,
+	'plugin' => [
+		'version' => '1.0.1',
+	],
+	'view_extensions' => [
+		'admin.css' => [
+			'css/scroll_to_top.css' => [],
+		],
+		'elgg.css' => [
+			'css/scroll_to_top.css' => [],
+		],
+		'page/elements/foot' => [
+			'scroll_to_top/fab' => [],
+		],
+		'js/elgg' => [
+			'js/scroll_to_top.js' => [],
+		],
+	],
 ];
